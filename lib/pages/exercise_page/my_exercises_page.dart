df296299e5ecf1775087aa412bbd919e9b4fca1d
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2022-02-25 20:34:58 +0100

    fixed no scrollable in MyExercise

--- lib/pages/exercise_page/my_exercises_page.dart
+++ lib/pages/exercise_page/my_exercises_page.dart
@@ -20,70 +20,72 @@ class MyExercisePage extends StatelessWidget {
       key: UniqueKey(),
       appBar: AppBar(title: const Text('Meine \u00DCbungen')),
       drawer: MyDrawer(),
-      body: Column(
-        children: <Widget>[
-          InformationObjectListWidget(
-            false,
-            objects: exerciseService.getAllExercisesFromUserOfType(user, 0),
-          ),
-          Stack(alignment: Alignment.topRight, children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
-              child: Divider(
-                thickness: 2,
-                color: Colors.green,
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            InformationObjectListWidget(
+              false,
+              objects: exerciseService.getAllExercisesFromUserOfType(user, 0),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Text("Aufw\u00E4rmen"),
-            ),
-          ]),
-          InformationObjectListWidget(
-            false,
-            objects: exerciseService.getAllExercisesFromUserOfType(user, 1),
-          ),
-          Stack(
-            alignment: Alignment.topRight,
-            children: [
+            Stack(alignment: Alignment.topRight, children: [
               Padding(
                 padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
                 child: Divider(
                   thickness: 2,
-                  color: Colors.deepOrange,
+                  color: Colors.green,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.only(right: 8.0),
-                child: Text("Training"),
+                child: Text("Aufw\u00E4rmen"),
               ),
-            ],
-          ),
-          InformationObjectListWidget(
-            false,
-            objects: exerciseService.getAllExercisesFromUserOfType(user, 2),
-          ),
-          Stack(
-            alignment: Alignment.topRight,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
-                child: Divider(
-                  thickness: 2,
-                  color: Colors.deepPurple,
+            ]),
+            InformationObjectListWidget(
+              false,
+              objects: exerciseService.getAllExercisesFromUserOfType(user, 1),
+            ),
+            Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.deepOrange,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Text("Dehnen"),
-              ),
-            ],
-          ),
-          InformationObjectListWidget(
-            false,
-            objects: exerciseService.getAllExercisesFromUserOfType(user, 3),
-          ),
-        ],
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text("Training"),
+                ),
+              ],
+            ),
+            InformationObjectListWidget(
+              false,
+              objects: exerciseService.getAllExercisesFromUserOfType(user, 2),
+            ),
+            Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text("Dehnen"),
+                ),
+              ],
+            ),
+            InformationObjectListWidget(
+              false,
+              objects: exerciseService.getAllExercisesFromUserOfType(user, 3),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: OpenContainer(
         closedColor: Theme.of(context).primaryColor,
